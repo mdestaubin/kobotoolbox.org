d84e26ceb59c706f1ca45eb28a85bfb864b09a58
pmusaraj <pmusaraj@gmail.com>
2018-05-25 14:38:48 -0400

    updated Gulp and plugins to latest

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,39 @@
-// Run `gulp watch`
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-var input = ['./scss/*.scss'];
-var output = './css';
-
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
-};
-
 gulp.task('sass', function () {
-  return gulp
-    .src(input)
-    .pipe(sass(sassOptions).on('error', sass.logError))
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(gulp.dest(output));
+    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', function() {
-  return gulp
-    .watch(input, ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('./scss/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', ['sass', 'watch']);
+// var input = ['./scss/*.scss'];
+// var output = './css';
+
+// var sassOptions = {
+//   errLogToConsole: true,
+//   outputStyle: 'expanded'
+// };
+
+// gulp.task('sass', function () {
+//   return gulp
+//     .src(input)
+//     .pipe(sass(sassOptions).on('error', sass.logError))
+//     .pipe(autoprefixer())
+//     .pipe(gulp.dest(output));
+// });
+
+// gulp.task('watch', function() {
+//   gulp.watch(input).on('change', function() {
+//   	gulp.parallel('sass');
+//   });
+// });
+
+// // gulp.task('default', ['sass', 'watch']);
+
